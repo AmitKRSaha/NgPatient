80b9933c8e5ffa7f1e66c656c04417d38e8b4cb8
AKSAHA <sheol.aks@gmail.com>
2019-02-04 18:05:52 +0530

    Added authentication for patient route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'patients', loadChildren: './dashboard/dashboard.module#DashboardModule'}
+  { path: 'patients', canActivate: [AuthGuard], loadChildren: './dashboard/dashboard.module#DashboardModule'}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,41 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  message: string;
+
+  constructor( public authService: AuthService, public router: Router) {
+    this.setMessage();
+  }
+
+  setMessage() {
+    this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+  }
+
 
   ngOnInit() {
   }
 
 
   login() {
-    this.router.navigateByUrl('/patients');
+    this.message = 'Trying to log in ...';
+
+    this.authService.login('/patients').subscribe(() => {
+      this.setMessage();
+      if (this.authService.isLoggedIn) {
+        // Get the redirect URL from our auth service
+        // If no redirect has been set, use the default
+        const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/login';
+
+        // Redirect the user
+        this.router.navigate([redirect]);
+      }
+    });
+    // this.router.navigateByUrl('/patients');
+  }
+
+  logout() {
+    this.authService.logout();
+    this.setMessage();
   }
 
 }
